918ccbdb3632fb6009efda0d2dd8365bbaf6773c
Uygar Polat <upolat@c2r2p13.hive.fi>
2025-05-12 11:30:18 +0300

    2025.05.12 added

--- 2025-05/2025.05.12-Easy.py
+++ 2025-05/2025.05.12-Easy.py
@@ -1,5 +1,40 @@
+"""
+2094. Finding 3-Digit Even Numbers
+
+You are given an integer array digits, where each element is a digit. The array may contain duplicates.
+
+You need to find all the unique integers that follow the given requirements:
+
+The integer consists of the concatenation of three elements from digits in any arbitrary order.
+The integer does not have leading zeros.
+The integer is even.
+For example, if the given digits were [1, 2, 3], integers 132 and 312 follow the requirements.
+
+Return a sorted array of the unique integers.
+
+Example 1:
+Input: digits = [2,1,3,0]
+Output: [102,120,130,132,210,230,302,310,312,320]
+Explanation: All the possible integers that follow the requirements are in the output array. 
+Notice that there are no odd integers or integers with leading zeros.
+
+Example 2:
+Input: digits = [2,2,8,8,2]
+Output: [222,228,282,288,822,828,882]
+Explanation: The same digit can be used as many times as it appears in digits. 
+In this example, the digit 8 is used twice each time in 288, 828, and 882.
+
+Example 3:
+Input: digits = [3,7,5]
+Output: []
+Explanation: No even integers can be formed using the given digits.
+ 
+Constraints:
+3 <= digits.length <= 100
+0 <= digits[i] <= 9
+"""
+
 from typing import List
-from itertools import permutations
 
 class Solution:
 	def findEvenNumbers(self, digits: List[int]) -> List[int]:
@@ -7,10 +42,12 @@ class Solution:
 
 		for num in range(100, 1000, 2):
 			new_num = list(map(int, str(num)))
+			flag = 0
 			for i in new_num:
-				if digits.count(i) < new_num.count(i):
-					break
-			result.extend(num)
+				if digits.count(i) >= new_num.count(i):
+					flag += 1
+			if flag == 3:
+				result.append(num)
 		return result
 	
 def main():
@@ -18,6 +55,14 @@ def main():
 	digits = [2,1,3,0]
 	result = solution.findEvenNumbers(digits)
 	print(f"Result is {result}") # Expected outcome: [102,120,130,132,210,230,302,310,312,320]
+
+	digits = [2,2,8,8,2]
+	result = solution.findEvenNumbers(digits)
+	print(f"Result is {result}") # Expected outcome: [222,228,282,288,822,828,882]
+
+	digits = [3,7,5]
+	result = solution.findEvenNumbers(digits)
+	print(f"Result is {result}") # Expected outcome: []
 
 if __name__ == "__main__":
 	main()
