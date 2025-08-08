6fab9dc9da0591f06c8d6fd35765791c73ab6309
uygarpolat <uygarpolat@gmail.com>
2025-08-08 20:31:44 +0300

    2025.02.09 added

---
+++ 2025-02/2025.02.09-Medium.py
@@ -0,0 +1,48 @@
+"""
+2364. Count Number of Bad Pairs
+
+You are given a 0-indexed integer array nums. A pair of indices (i, j) is a bad pair if i < j and j - i != nums[j] - nums[i].
+
+Return the total number of bad pairs in nums.
+
+Example 1:
+Input: nums = [4,1,3,3]
+Output: 5
+Explanation: The pair (0, 1) is a bad pair since 1 - 0 != 1 - 4.
+The pair (0, 2) is a bad pair since 2 - 0 != 3 - 4, 2 != -1.
+The pair (0, 3) is a bad pair since 3 - 0 != 3 - 4, 3 != -1.
+The pair (1, 2) is a bad pair since 2 - 1 != 3 - 1, 1 != 2.
+The pair (2, 3) is a bad pair since 3 - 2 != 3 - 3, 1 != 0.
+There are a total of 5 bad pairs, so we return 5.
+
+Example 2:
+Input: nums = [1,2,3,4,5]
+Output: 0
+Explanation: There are no bad pairs.
+ 
+Constraints:
+1 <= nums.length <= 10^5
+1 <= nums[i] <= 10^9
+"""
+from typing import List
+from collections import defaultdict
+
+class Solution:
+	def countBadPairs(self, nums: List[int]) -> int:
+		tally = defaultdict(int)
+		pairs = 0
+
+		for i, num in enumerate(nums):
+			pairs += tally[num - i]
+			tally[num - i] += 1
+
+		return (len(nums) * (len(nums)-1)) // 2 - pairs
+
+def main():
+	solution = Solution()
+	assert solution.countBadPairs([4,1,3,3]) == 5
+	assert solution.countBadPairs([1,2,3,4,5])== 0
+	print("✅ All tests passed!")
+
+if __name__ == "__main__":
+	main()
