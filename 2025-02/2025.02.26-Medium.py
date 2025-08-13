a287f547b9c0718a861293acd70c695459089fe0
uygarpolat <uygarpolat@gmail.com>
2025-08-13 22:52:59 +0300

    2025.02.26 added

---
+++ 2025-02/2025.02.26-Medium.py
@@ -0,0 +1,55 @@
+"""
+1749. Maximum Absolute Sum of Any Subarray
+
+You are given an integer array nums. The absolute sum of a subarray [numsl, numsl+1, ..., numsr-1, numsr] is abs(numsl + numsl+1 + ... + numsr-1 + numsr).
+
+Return the maximum absolute sum of any (possibly empty) subarray of nums.
+
+Note that abs(x) is defined as follows:
+
+If x is a negative integer, then abs(x) = -x.
+If x is a non-negative integer, then abs(x) = x.
+
+Example 1:
+Input: nums = [1,-3,2,3,-4]
+Output: 5
+Explanation: The subarray [2,3] has absolute sum = abs(2+3) = abs(5) = 5.
+
+Example 2:
+Input: nums = [2,-5,1,-4,3,-2]
+Output: 8
+Explanation: The subarray [-5,1,-4] has absolute sum = abs(-5+1-4) = abs(-8) = 8.
+
+Constraints:
+1 <= nums.length <= 10^5
+-10^4 <= nums[i] <= 10^4
+"""
+from typing import List
+
+class Solution:
+	def maxAbsoluteSum(self, nums: List[int]) -> int:
+		
+		maxEnd = 0
+		maxRes = 0
+		for num in nums:
+			maxEnd += num
+			maxRes = max(maxRes, maxEnd)
+			maxEnd = max(0, maxEnd)
+
+		minEnd = 0
+		minRes = 0
+		for num in nums:
+			minEnd += num
+			minRes = min(minRes, minEnd)
+			minEnd = min(0, minEnd)
+		
+		return max(maxRes, abs(minRes))
+
+def main():
+	solution = Solution()
+	assert solution.maxAbsoluteSum([1,-3,2,3,-4]) == 5
+	assert solution.maxAbsoluteSum([2,-5,1,-4,3,-2]) == 8
+	print("✅ All tests passed!")
+
+if __name__ == "__main__":
+	main()
