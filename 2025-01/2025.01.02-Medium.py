e12c978fb809d1462dc5648ca152166c19cedb6a
uygarpolat <uygarpolat@gmail.com>
2025-08-14 16:48:13 +0300

    2025.01.02 added

---
+++ 2025-01/2025.01.02-Medium.py
@@ -0,0 +1,60 @@
+"""
+2559. Count Vowel Strings in Ranges
+
+You are given a 0-indexed array of strings words and a 2D array of integers queries.
+
+Each query queries[i] = [li, ri] asks us to find the number of strings present at the indices ranging from li to ri (both inclusive) of words that start and end with a vowel.
+
+Return an array ans of size queries.length, where ans[i] is the answer to the ith query.
+
+Note that the vowel letters are 'a', 'e', 'i', 'o', and 'u'.
+
+Example 1:
+Input: words = ["aba","bcb","ece","aa","e"], queries = [[0,2],[1,4],[1,1]]
+Output: [2,3,0]
+Explanation: The strings starting and ending with a vowel are "aba", "ece", "aa" and "e".
+The answer to the query [0,2] is 2 (strings "aba" and "ece").
+to query [1,4] is 3 (strings "ece", "aa", "e").
+to query [1,1] is 0.
+We return [2,3,0].
+
+Example 2:
+Input: words = ["a","e","i"], queries = [[0,2],[0,1],[2,2]]
+Output: [3,2,1]
+Explanation: Every string satisfies the conditions, so we return [3,2,1].
+
+Constraints:
+1 <= words.length <= 10^5
+1 <= words[i].length <= 40
+words[i] consists only of lowercase English letters.
+sum(words[i].length) <= 3 * 10^5
+1 <= queries.length <= 10^5
+0 <= li <= ri < words.length
+"""
+from typing import List
+
+class Solution:
+	def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+
+		result = [0] * len(queries)
+		lookup = [0] * (len(words) + 1)
+		vowels = set("aeiou")
+
+		for i in range(len(words)):
+			lookup[i+1] = lookup[i]
+			if words[i][0] in vowels and words[i][-1] in vowels:
+				lookup[i+1] += 1
+
+		for i, (start, end) in enumerate(queries):
+			result[i] = lookup[end+1] - lookup[start]
+
+		return result
+	
+def main():
+	solution = Solution()
+	assert solution.vowelStrings(["aba","bcb","ece","aa","e"], [[0,2],[1,4],[1,1]]) == [2,3,0]
+	assert solution.vowelStrings(["a","e","i"], [[0,2],[0,1],[2,2]]) == [3,2,1]
+	print("✅ All tests passed!")
+
+if __name__ == "__main__":
+	main()
