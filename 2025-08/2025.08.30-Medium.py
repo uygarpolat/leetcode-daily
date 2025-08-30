c3986943997efa36d3741ebdfd0a9442f1424cd1
uygarpolat <uygarpolat@gmail.com>
2025-08-30 09:47:13 +0300

    2025.08.30 added

---
+++ 2025-08/2025.08.30-Medium.py
@@ -0,0 +1,97 @@
+"""
+36. Valid Sudoku
+
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+Example 1:
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Example 2:
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+Constraints:
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+"""
+from typing import List
+
+class Solution:
+	def isValidSudoku(self, board: List[List[str]]) -> bool:
+		n = len(board)
+		rows = [[False] * (n+1) for _ in range(n+1)]
+		cols = [[False] * (n+1) for _ in range(n+1)]
+		squares = [[False] * (n+1) for _ in range(n+1)]
+
+		for i in range(n):
+			for j in range(n):
+				value = board[i][j]
+				if not value.isnumeric():
+					continue
+				value = int(value)
+				square = i - (i % 3) + j // 3
+				if rows[i][value] or cols[j][value] or squares[square][value]:
+					return False
+				rows[i][value] = True
+				cols[j][value] = True
+				squares[square][value] = True
+
+		return True
+
+def main():
+	solution = Solution()
+
+	assert solution.isValidSudoku(
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]) == True
+	
+	assert solution.isValidSudoku(
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]) == False
+	print("✅ All tests passed!")
+
+if __name__ == "__main__":
+	main()
