2170dabe5d28e6b5ae4b2c92422077401a8321e8
uygarpolat <uygarpolat@gmail.com>
2025-09-05 19:54:30 +0300

    2025.09.05 added

---
+++ 2025-09/2025.09.05-Medium.py
@@ -0,0 +1,47 @@
+"""
+2749. Minimum Operations to Make the Integer Zero
+
+You are given two integers num1 and num2.
+
+In one operation, you can choose integer i in the range [0, 60] and subtract 2i + num2 from num1.
+
+Return the integer denoting the minimum number of operations needed to make num1 equal to 0.
+
+If it is impossible to make num1 equal to 0, return -1.
+
+Example 1:
+Input: num1 = 3, num2 = -2
+Output: 3
+Explanation: We can make 3 equal to 0 with the following operations:
+- We choose i = 2 and subtract 22 + (-2) from 3, 3 - (4 + (-2)) = 1.
+- We choose i = 2 and subtract 22 + (-2) from 1, 1 - (4 + (-2)) = -1.
+- We choose i = 0 and subtract 20 + (-2) from -1, (-1) - (1 + (-2)) = 0.
+It can be proven, that 3 is the minimum number of operations that we need to perform.
+
+Example 2:
+Input: num1 = 5, num2 = 7
+Output: -1
+Explanation: It can be proven, that it is impossible to make 5 equal to 0 with the given operation.
+
+Constraints:
+1 <= num1 <= 10^9
+-109 <= num2 <= 10^9
+"""
+class Solution:
+	def makeTheIntegerZero(self, num1: int, num2: int) -> int:
+		for k in range(1, 61):
+			S = num1 - k * num2
+			if S <= 0 and num2 > 0:
+				break
+			elif k <= S and S.bit_count() <= k:
+				return k
+		return -1
+
+def main():
+	solution = Solution()
+	assert solution.makeTheIntegerZero(3, -2) == 3
+	assert solution.makeTheIntegerZero(5, 7) == -1
+	print("✅ All tests passed!")
+
+if __name__ == "__main__":
+	main()
