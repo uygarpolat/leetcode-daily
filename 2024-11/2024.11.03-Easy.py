a3df4353c5ecc1afc29bfe79c3606518522bfb81
uygarpolat <uygarpolat@gmail.com>
2025-09-09 02:00:12 +0300

    2024.11.03 added

---
+++ 2024-11/2024.11.03-Easy.py
@@ -0,0 +1,33 @@
+"""
+796. Rotate String
+
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+
+Example 1:
+Input: s = "abcde", goal = "cdeab"
+Output: true
+
+Example 2:
+Input: s = "abcde", goal = "abced"
+Output: false
+
+Constraints:
+1 <= s.length, goal.length <= 100
+s and goal consist of lowercase English letters.
+"""
+class Solution:
+	def rotateString(self, s: str, goal: str) -> bool:
+		return len(s) == len(goal) and goal in (s + s)
+
+def main():
+	solution = Solution()
+	assert solution.rotateString("abcde", "cdeab") == True
+	assert solution.rotateString("abcde", "abced") == False
+	print("✅ All tests passed!")
+
+if __name__ == "__main__":
+	main()
