aba6530945df589479d9a08d6e2f8267c4459b58
uygarpolat <uygarpolat@gmail.com>
2026-01-06 19:36:09 +0200

    2026.01.06 added

---
+++ 2026-01/2026.01.06-Medium.py
@@ -0,0 +1,65 @@
+"""
+1161. Maximum Level Sum of a Binary Tree
+
+Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+
+Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+
+Example 1:
+Input: root = [1,7,0,7,-8,null,null]
+Output: 2
+Explanation:
+Level 1 sum = 1.
+Level 2 sum = 7 + 0 = 7.
+Level 3 sum = 7 + -8 = -1.
+So we return the level with the maximum sum which is level 2.
+
+Example 2:
+Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
+Output: 2
+
+Constraints:
+The number of nodes in the tree is in the range [1, 10^4].
+-10^5 <= Node.val <= 10^5
+"""
+
+from typing import Optional
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        dq = deque([root])
+        result = (float("-inf"), 0, 0)
+        level = 0
+        while dq:
+            local_result = 0
+            len_dq = len(dq)
+            level += 1
+            for i in range(len_dq):
+                curr = dq.popleft()
+                local_result += curr.val
+                if curr.left:
+                    dq.append(curr.left)
+                if curr.right:
+                    dq.append(curr.right)
+            result = max(result, (local_result, -level, level))
+        return result[2]
+
+
+def main():
+    node = TreeNode(1, TreeNode(7, TreeNode(7), TreeNode(-8)), TreeNode(0))
+    solution = Solution()
+    assert solution.maxLevelSum(node) == 2
+    print("✅ All tests passed!")
+
+
+if __name__ == "__main__":
+    main()
